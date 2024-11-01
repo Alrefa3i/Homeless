273c3407857f165d107d573ee3c504198ce117ce
Morrola <moradjamos56@gmail.com>
2024-11-01 16:24:17 +0300

    add backend

---
+++ Backend/BackendProject/review.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeLess.Models
+{
+    public class Review
+    {
+        public int UserId { get; set; }
+        public string text { get; set; }
+        public DateTime date { get; set; }
+
+        public User User { get; set; }
+        public product product { get; set; }
+    }
+}
