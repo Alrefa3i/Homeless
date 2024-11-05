1c9060477102b2e74687fc5279b6b9cde1300513
osama Tubaishat <Osamanaser006@gmail.com>
2024-11-05 11:27:45 +0300

    About us

---
+++ Backend/HomeLess/HomeLess/Controllers/AboutUsController.cs
@@ -0,0 +1,26 @@
+﻿using HomeLess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLess.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private readonly AppDBContaxt _context;
+
+        public AboutUsController(AppDBContaxt context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            /*var testimonials = _context.Testimonials
+                .Where(t => t.Rating >= 4) 
+                .OrderByDescending(t => t.date)
+                .ToList();*/
+            var testimonials = _context.Testimonials.Include(t => t.User).ToList();
+            return View(testimonials);
+        }
+    }
+}
--- Backend/HomeLess/HomeLess/obj/Debug/net8.0/HomeLess.AssemblyInfo.cs
+++ Backend/HomeLess/HomeLess/obj/Debug/net8.0/HomeLess.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeLess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706dc8bbe73da58d376868b24beb042f72e40c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa1ee02a75e5e6bdff94025ad0d47afc4a43ecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeLess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeLess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
